1342e33906d352248c89661331cbb27ce23b6c3a
michel428 <michel@waf-forme.fr>
2026-07-23 17:36:11 +0200

    Update BootReceiver.java

--- app/src/main/java/fr/wafforme/signage/BootReceiver.java
+++ app/src/main/java/fr/wafforme/signage/BootReceiver.java
@@ -4,16 +4,15 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-/**
- * Lance l'app automatiquement au démarrage de la box.
- */
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction()) ||
             "android.intent.action.QUICKBOOT_POWERON".equals(intent.getAction())) {
             Intent launch = new Intent(context, MainActivity.class);
-            launch.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            launch.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_CLEAR_TOP
+                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
             context.startActivity(launch);
         }
     }
